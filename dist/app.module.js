bed5cc38e2a5f67c541ab24574653a93d315d404
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-08-11 07:06:48 +0100

    task: Init auth local strategy

--- dist/app.module.js
+++ dist/app.module.js
@@ -11,11 +11,12 @@ const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 const auth_module_1 = require("./auth/auth.module");
+const users_module_1 = require("./users/users.module");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     common_1.Module({
-        imports: [auth_module_1.AuthModule],
+        imports: [auth_module_1.AuthModule, users_module_1.UsersModule],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
